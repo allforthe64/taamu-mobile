8fa46a2bf679645102c4768c62f6e326a54f15d5
allforthe64 <williamhrainey@gmail.com>
2024-11-19 13:54:27 -0800

    added scrollview to homepage/created stylesheet

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <ScrollView style={}>
+
+    </ScrollView>
   )
 }
 
 export default index
+
+const styles = StyleSheet.create({
+  mainCon: {
+    backgroundColor: '#01354B',
+    width: '100%'
+  }
+})
