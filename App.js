edecbdcdc0928fc5d935c4e1b7b0d8d56bd86798
allforthe64 <williamhrainey@gmail.com>
2024-11-24 16:31:30 -0800

    removed the sentry initialization I still had in app.js

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Sentry from '@sentry/react-native';
-
-Sentry.init({
-  dsn: 'https://78cc57d36d498aebafe0ee5f0448b6e8@o4507346968772608.ingest.us.sentry.io/4508355887759360',
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // enableSpotlight: __DEV__,
-});
 
 export default function App() {
 
