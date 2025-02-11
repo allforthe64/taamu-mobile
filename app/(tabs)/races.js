9a323e20bdefab8e15a00f4bb075d820945cf91f
allforthe64 <williamhrainey@gmail.com>
2025-02-11 11:49:30 -0800

    setup hook to grab the race data/setup logic for the isLoading setup

--- app/(tabs)/races.js
+++ app/(tabs)/races.js
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+//react/react related hooks
+import React, { useState, useCallback } from 'react'
+import { useFocusEffect } from 'expo-router'
+
+//firebase hook imports
+import { getAllRaces } from '../firebase/firestore'
 
 const races = () => {
+
+  //initialize state
+  const [races, setRaces] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  //get race data
+  useFocusEffect(useCallback(() => {
+    setIsLoading(true)
+
+    const getRacesData = async () => {
+      const allRacesData = await getAllRaces()
+      setRaces(allRacesData)
+    }
+    getRacesData() 
+    setIsLoading(false)
+  }, []))
+
+  console.log(races)
+
   return (
     <View>
-      <Text>races</Text>
+      {isLoading ?
+        <View style={styles.loadingContainer}>
+          <View style={styles.loadingIndicatorContainer}>
+            <ActivityIndicator color={'#09CAC7'}/>
+            <Text style={styles.loadingText}>Loading...</Text>
+          </View>
+        </View>
+      :
+        <View style={styles.mainContainer}>
+          
+        </View>
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+  //isLoading styles
+  loadingContainer: {
+      width: '100%',
+      backgroundColor: 'white',
+      flex: 1,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center'
+  },
+  loadingIndicatorContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  loadingText: {
+    color: '#09CAC7',
+    fontSize: 20,
+    marginLeft: '5%'
+  },
+
+  //main races page styles
+  mainContainer: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#01354B'
+  }
+})
 
 export default races
