8c02984863dacdcab0c4b5dc2ab3cef20b53c9a2
allforthe64 <williamhrainey@gmail.com>
2025-02-13 14:35:31 -0800

    added in filters

--- app/components/racesPage/RaceList.jsx
+++ app/components/racesPage/RaceList.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const RaceList = () => {
+const RaceList = ({races}) => {
+
+  console.log(races)
+
   return (
     <View>
       <Text>RaceList</Text>
--- app/components/racesPage/RacesMain.jsx
+++ app/components/racesPage/RacesMain.jsx
@@ -1,16 +1,20 @@
 import { View, Text } from 'react-native'
 
 //react/react related imports
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { useFocusEffect } from 'expo-router'
 
 //getKey data
 import { getKey } from '../../firebase/firestore'
 
+//component imports
+import RaceList from './RaceList'
+
 const RacesMain = ({races}) => {
 
     //initialize state
     const [decryptedRaces, setDecryptedRaces] = useState([])
+    const [filteredRaces, setFilteredRaces] = useState([])
     const [raceTypeFilter, setRaceTypeFilter] = useState('All Race Types')
     const [craftTypeFilter, setCraftTypeFilter] = useState('')
     const [distanceFilter, setDistanceFilter] = useState({value: {}, index: '0'})
@@ -70,11 +74,90 @@ const RacesMain = ({races}) => {
         }, [races, keyData])
     )
 
-    console.log(decryptedRaces)
+    useEffect(() => {
+        if (decryptedRaces.length > 0) {
+            let newRaceArray = decryptedRaces.filter(race => !race.draft)
+
+            //initially filter for races with a matching org name or race title
+            if (query !== '') {
+                newRaceArray = newRaceArray.filter(race => {
+                    if (race.orgName.toLowerCase().includes(query.toLowerCase()) || race.title.toLowerCase().includes(query.toLowerCase())) {
+                        return race
+                    }
+                })
+            }
+
+            //filter for races matching the race type
+            if (raceTypeFilter !== 'All Race Types') {
+                newRaceArray = newRaceArray.filter(race => race.raceType === raceTypeFilter)
+            }   
+
+            //filter for races that include the selected boat type
+            if (craftTypeFilter !== 'All Boats' && craftTypeFilter !== '') {
+                newRaceArray = newRaceArray.filter(race => race.craftCategories.includes(craftTypeFilter))
+            }
+
+            //filter for races that include the selected distance
+            if (distanceFilter.index !== '0') {
+                
+                newRaceArray = newRaceArray.filter(race => {
+                    let passed = false
+
+                    //single distance
+                    if (distanceFilter.value.value) {
+                        race.raceEvents.forEach(event => {
+                            if (Number(event.distance) === distanceFilter.value.value && event.distanceMetric === distanceFilter.value.metric) {
+                                passed = true
+                            }
+                        })
+                    } 
+                    //distance range
+                    else {
+                        race.raceEvents.forEach(event => {
+                            if (Number(event.distance) >= distanceFilter.value.lower && Number(event.distance) <= distanceFilter.value.upper && event.distanceMetric === distanceFilter.value.metric) {
+                                passed = true
+                            }
+                        })
+                    }
+                    if (passed) return race
+                })
+            }
+
+            //if theres a start and end date filter for races that fall within those dates
+            if (startDate && endDate) {
+                newRaceArray = newRaceArray.filter(race => {
+                    if (new Date(race.startDate) >= new Date(startDate) && new Date(race.endDate) <= new Date(endDate))
+                    return race   
+                })
+            } 
+            //if theres only a startDate filter for races that occur after the selected startDate
+            else if (startDate) {
+                newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(startDate))
+            } 
+            //if theres only a endDate filter for races that occur before the selected startDate
+            else if (endDate) {
+                newRaceArray = newRaceArray.filter(race => new Date(race.endDate) <= new Date(endDate))
+            }
+
+            //results vs ongoing results vs registration
+                if (timeFilter === 'upcoming') {
+                    newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(currentDate) && new Date(race.endDate) >= new Date(currentDate))
+                }
+                else if (timeFilter === 'ongoing') {
+                    newRaceArray = newRaceArray.filter(race => new Date(currentDate) >= new Date(race.startDate) && new Date(currentDate) <= new Date(race.endDate))
+                }
+                else if (timeFilter === 'results') {
+                    newRaceArray = newRaceArray.filter(race => new Date(race.startDate) < new Date(currentDate) &&  new Date(race.endDate) <= new Date(currentDate))
+                }
+
+
+            setFilteredRaces(newRaceArray)
+        }
+    }, [decryptedRaces, raceTypeFilter, craftTypeFilter, distanceFilter, startDate, endDate, timeFilter, query])
 
   return (
     <View>
-      <Text>RacesMain</Text>
+      <RaceList races={filteredRaces}/>
     </View>
   )
 }
