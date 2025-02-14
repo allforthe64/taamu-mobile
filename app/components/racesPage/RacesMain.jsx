1cdda874d3a898bec3d26ba2615aa431853f0576
allforthe64 <williamhrainey@gmail.com>
2025-02-14 12:16:57 -0800

    logging data out of the fetch request

--- app/components/racesPage/RacesMain.jsx
+++ app/components/racesPage/RacesMain.jsx
@@ -58,6 +58,7 @@ const RacesMain = ({races}) => {
                             // Check if the response was successful
                             if (response.ok) {
                                 const data = await response.json();
+                                console.log('data: ', data)
                                 setDecryptedRaces(data)
                             } else {
                                 console.error('Failed to send data:', response.status);
@@ -74,92 +75,87 @@ const RacesMain = ({races}) => {
         }, [races, keyData])
     )
 
-    useFocusEffect(
-        useCallback(() => {
-            if (decryptedRaces.length > 0) {
-                alert('running')
-                let newRaceArray = decryptedRaces.filter(race => !race.draft)
+    useEffect(() => {
+        if (decryptedRaces.length > 0) {
+            alert('running')
+            let newRaceArray = decryptedRaces.filter(race => !race.draft)
 
-                //initially filter for races with a matching org name or race title
-                if (query !== '') {
-                    newRaceArray = newRaceArray.filter(race => {
-                        if (race.orgName.toLowerCase().includes(query.toLowerCase()) || race.title.toLowerCase().includes(query.toLowerCase())) {
-                            return race
-                        }
-                    })
-                }
-
-                //filter for races matching the race type
-                if (raceTypeFilter !== 'All Race Types') {
-                    newRaceArray = newRaceArray.filter(race => race.raceType === raceTypeFilter)
-                }   
-
-                //filter for races that include the selected boat type
-                if (craftTypeFilter !== 'All Boats' && craftTypeFilter !== '') {
-                    newRaceArray = newRaceArray.filter(race => race.craftCategories.includes(craftTypeFilter))
-                }
-
-                //filter for races that include the selected distance
-                if (distanceFilter.index !== '0') {
-                    
-                    newRaceArray = newRaceArray.filter(race => {
-                        let passed = false
-
-                        //single distance
-                        if (distanceFilter.value.value) {
-                            race.raceEvents.forEach(event => {
-                                if (Number(event.distance) === distanceFilter.value.value && event.distanceMetric === distanceFilter.value.metric) {
-                                    passed = true
-                                }
-                            })
-                        } 
-                        //distance range
-                        else {
-                            race.raceEvents.forEach(event => {
-                                if (Number(event.distance) >= distanceFilter.value.lower && Number(event.distance) <= distanceFilter.value.upper && event.distanceMetric === distanceFilter.value.metric) {
-                                    passed = true
-                                }
-                            })
-                        }
-                        if (passed) return race
-                    })
-                }
-
-                //if theres a start and end date filter for races that fall within those dates
-                if (startDate && endDate) {
-                    newRaceArray = newRaceArray.filter(race => {
-                        if (new Date(race.startDate) >= new Date(startDate) && new Date(race.endDate) <= new Date(endDate))
-                        return race   
-                    })
-                } 
-                //if theres only a startDate filter for races that occur after the selected startDate
-                else if (startDate) {
-                    newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(startDate))
-                } 
-                //if theres only a endDate filter for races that occur before the selected startDate
-                else if (endDate) {
-                    newRaceArray = newRaceArray.filter(race => new Date(race.endDate) <= new Date(endDate))
-                }
-
-                //results vs ongoing results vs registration
-                    if (timeFilter === 'upcoming') {
-                        newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(currentDate) && new Date(race.endDate) >= new Date(currentDate))
+            //initially filter for races with a matching org name or race title
+            if (query !== '') {
+                newRaceArray = newRaceArray.filter(race => {
+                    if (race.orgName.toLowerCase().includes(query.toLowerCase()) || race.title.toLowerCase().includes(query.toLowerCase())) {
+                        return race
                     }
-                    else if (timeFilter === 'ongoing') {
-                        newRaceArray = newRaceArray.filter(race => new Date(currentDate) >= new Date(race.startDate) && new Date(currentDate) <= new Date(race.endDate))
-                    }
-                    else if (timeFilter === 'results') {
-                        newRaceArray = newRaceArray.filter(race => new Date(race.startDate) < new Date(currentDate) &&  new Date(race.endDate) <= new Date(currentDate))
-                    }
-
-
-                setFilteredRaces(newRaceArray)
+                })
             }
-        }, [decryptedRaces, raceTypeFilter, craftTypeFilter, distanceFilter, startDate, endDate, timeFilter, query])
-    )
 
-    console.log('decrypted races: ', decryptedRaces)
-    console.log('decrypted races length: ', decryptedRaces.length)
+            //filter for races matching the race type
+            if (raceTypeFilter !== 'All Race Types') {
+                newRaceArray = newRaceArray.filter(race => race.raceType === raceTypeFilter)
+            }   
+
+            //filter for races that include the selected boat type
+            if (craftTypeFilter !== 'All Boats' && craftTypeFilter !== '') {
+                newRaceArray = newRaceArray.filter(race => race.craftCategories.includes(craftTypeFilter))
+            }
+
+            //filter for races that include the selected distance
+            if (distanceFilter.index !== '0') {
+                
+                newRaceArray = newRaceArray.filter(race => {
+                    let passed = false
+
+                    //single distance
+                    if (distanceFilter.value.value) {
+                        race.raceEvents.forEach(event => {
+                            if (Number(event.distance) === distanceFilter.value.value && event.distanceMetric === distanceFilter.value.metric) {
+                                passed = true
+                            }
+                        })
+                    } 
+                    //distance range
+                    else {
+                        race.raceEvents.forEach(event => {
+                            if (Number(event.distance) >= distanceFilter.value.lower && Number(event.distance) <= distanceFilter.value.upper && event.distanceMetric === distanceFilter.value.metric) {
+                                passed = true
+                            }
+                        })
+                    }
+                    if (passed) return race
+                })
+            }
+
+            //if theres a start and end date filter for races that fall within those dates
+            if (startDate && endDate) {
+                newRaceArray = newRaceArray.filter(race => {
+                    if (new Date(race.startDate) >= new Date(startDate) && new Date(race.endDate) <= new Date(endDate))
+                    return race   
+                })
+            } 
+            //if theres only a startDate filter for races that occur after the selected startDate
+            else if (startDate) {
+                newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(startDate))
+            } 
+            //if theres only a endDate filter for races that occur before the selected startDate
+            else if (endDate) {
+                newRaceArray = newRaceArray.filter(race => new Date(race.endDate) <= new Date(endDate))
+            }
+
+            //results vs ongoing results vs registration
+                if (timeFilter === 'upcoming') {
+                    newRaceArray = newRaceArray.filter(race => new Date(race.startDate) >= new Date(currentDate) && new Date(race.endDate) >= new Date(currentDate))
+                }
+                else if (timeFilter === 'ongoing') {
+                    newRaceArray = newRaceArray.filter(race => new Date(currentDate) >= new Date(race.startDate) && new Date(currentDate) <= new Date(race.endDate))
+                }
+                else if (timeFilter === 'results') {
+                    newRaceArray = newRaceArray.filter(race => new Date(race.startDate) < new Date(currentDate) &&  new Date(race.endDate) <= new Date(currentDate))
+                }
+
+
+            setFilteredRaces(newRaceArray)
+        }
+    }, [decryptedRaces, raceTypeFilter, craftTypeFilter, distanceFilter, startDate, endDate, timeFilter, query])
 
   return (
     <View>
