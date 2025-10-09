4d085861be417eb3ee42a6302fae2940c91dddaf
allforthe64 <williamhrainey@gmail.com>
2025-10-09 15:18:43 -0700

    uncommented ThumbnailAndInfo component

--- app/(tabs)/races/[raceId].js
+++ app/(tabs)/races/[raceId].js
@@ -131,8 +131,8 @@ const RacePage = () => {
   return (
     <View style={styles.mainContainer}>
       <ScrollView>
-        {/* <ThumbnailAndInfo thumbnailURL={thumbnail} raceData={race} organizerData={organizer} setRegistrationWindowOpen={setRegistrationWindowOpen} currentUser={currentUser} raceId={raceId} setViewParticipants={setViewParticipants} keyData={keyData}/>
-        <DetailsAndRegistrationButton desc={race?.description} currentUser={currentUser} startTimes={race?.startTimes} endTimes={race?.endTimes} setRegistrationWindowOpen={setRegistrationWindowOpen} raceData={race}/> */}
+        <ThumbnailAndInfo thumbnailURL={thumbnail} raceData={race} organizerData={organizer} setRegistrationWindowOpen={setRegistrationWindowOpen} currentUser={currentUser} raceId={raceId} setViewParticipants={setViewParticipants} keyData={keyData}/>
+        {/* <DetailsAndRegistrationButton desc={race?.description} currentUser={currentUser} startTimes={race?.startTimes} endTimes={race?.endTimes} setRegistrationWindowOpen={setRegistrationWindowOpen} raceData={race}/> */}
       </ScrollView>
     </View>
   )
