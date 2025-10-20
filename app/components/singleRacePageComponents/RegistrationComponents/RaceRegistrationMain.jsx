05459d5646cdad80f772cfa087543077f9a4ac56
allforthe64 <williamhrainey@gmail.com>
2025-10-20 12:29:10 -0700

    added useState import to RaceRegistrationMain component

--- app/components/singleRacePageComponents/RegistrationComponents/RaceRegistrationMain.jsx
+++ app/components/singleRacePageComponents/RegistrationComponents/RaceRegistrationMain.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Dimensions, ScrollView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 //fontAwesome imports
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
