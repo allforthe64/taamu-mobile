a0275b5a1717a5c51cc90b57c1dccb48e9e27edb
allforthe64 <williamhrainey@gmail.com>
2025-10-21 09:58:27 -0700

    added useState import to RegistrationComponentRaceInfo.jsx

--- app/components/singleRacePageComponents/RegistrationComponents/RegistrationComponentRaceInfo.jsx
+++ app/components/singleRacePageComponents/RegistrationComponents/RegistrationComponentRaceInfo.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegistrationComponentRaceInfo = ({ keyData, organizerData, raceData }) => {
 
